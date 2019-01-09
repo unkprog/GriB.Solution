fed6917a9a261ab42ab78a0b3cd162074df20d10
Sergey Bezhnar <unkprog@gmail.com>
2019-01-09 09:55:02 +0300

    Добавление части функционала по редактированию документов движения товара

--- GriB.Client.App/Content/js/app/controller/document/editor/editor.js
+++ GriB.Client.App/Content/js/app/controller/document/editor/editor.js
@@ -11,7 +11,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-define(["require", "exports", "app/common/basecontroller", "app/services/documentservice"], function (require, exports, base, svc) {
+define(["require", "exports", "app/common/basecontroller", "app/services/documentservice", "app/services/settingsservice", "app/common/variables"], function (require, exports, base, svc, svcSetting, vars) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var Controller;
@@ -34,6 +34,15 @@ define(["require", "exports", "app/common/basecontroller", "app/services/documen
                         enumerable: true,
                         configurable: true
                     });
+                    Object.defineProperty(Editor.prototype, "SettingService", {
+                        get: function () {
+                            if (!this.settingService)
+                                this.settingService = new svcSetting.Services.SettingsService();
+                            return this.settingService;
+                        },
+                        enumerable: true,
+                        configurable: true
+                    });
                     Editor.prototype.createOptions = function () {
                         return { Url: "/Content/view/document/editor/document.html", Id: "document-view" };
                     };
@@ -41,6 +50,15 @@ define(["require", "exports", "app/common/basecontroller", "app/services/documen
                         var oo = new kendo.data.ObservableObject({
                             "Header": this.Header,
                             "editModel": {},
+                            "conduct": true,
+                            "labelConduct": vars._statres("label$conduct"),
+                            "labelDate": vars._statres("label$date"),
+                            "labelSalepoint": vars._statres("label$salePoint"),
+                            "labelName": vars._statres("label$name"),
+                            "labelQuantityShort": vars._statres("label$quantityshort"),
+                            "labelUnitShort": vars._statres("label$unitshort"),
+                            "labelSum": vars._statres("label$sum"),
+                            "labelAdd": vars._statres("button$label$add")
                         });
                         return oo;
                     };
@@ -67,6 +85,10 @@ define(["require", "exports", "app/common/basecontroller", "app/services/documen
                         return result;
                     };
                     Editor.prototype.ViewInit = function (view) {
+                        this.dateControl = view.find("#document-view-date");
+                        this.dateControl.datepicker();
+                        this.salePointControl = view.find("#document-view-salepoint-row");
+                        this.positionRows = view.find("#product-position-rows");
                         return _super.prototype.ViewInit.call(this, view);
                     };
                     Editor.prototype.ViewShow = function (e) {
@@ -74,8 +96,16 @@ define(["require", "exports", "app/common/basecontroller", "app/services/documen
                     };
                     Editor.prototype.createEvents = function () {
                         _super.prototype.createEvents.call(this);
+                        this.SalePointButtonClick = this.createTouchClickEvent(this.salePointControl, this.salePointButtonClick);
+                        this.Model.bind("change", $.proxy(this.changeModel, this));
                     };
                     Editor.prototype.destroyEvents = function () {
+                        this.Model.unbind("change");
+                        if (this.btnAddPosition)
+                            this.destroyTouchClickEvent(this.btnAddPosition, this.addPositionButtonClick);
+                        if (this.btnRemovePosition)
+                            this.destroyTouchClickEvent(this.btnRemovePosition, this.removePositionButtonClick);
+                        this.destroyTouchClickEvent(this.salePointControl, this.SalePointButtonClick);
                         _super.prototype.destroyEvents.call(this);
                     };
                     Editor.prototype.ViewResize = function (e) {
@@ -83,9 +113,91 @@ define(["require", "exports", "app/common/basecontroller", "app/services/documen
                     };
                     Editor.prototype.afterLoad = function (responseData) {
                         _super.prototype.afterLoad.call(this, responseData);
-                        this.setupPositions(responseData);
+                        this.setupPositions();
                     };
-                    Editor.prototype.setupPositions = function (responseData) {
+                    Editor.prototype.changeModel = function (e) {
+                    };
+                    Editor.prototype.setupPositions = function () {
+                        var self = this;
+                        var model = this.EditorModel;
+                        var data = model.positions;
+                        var html = '';
+                        if (this.btnAddPosition)
+                            this.destroyTouchClickEvent(this.btnAddPosition, this.addPositionButtonClick);
+                        this.positionRows.unbind();
+                        if (data && data.length > 0) {
+                            for (var i = 0, icount = (data && data.length ? data.length : 0); i < icount; i++) {
+                                data[i].sum = Math.round((data[i].quantity * data[i].product.sellingprice) * 100) / 100;
+                                html += '<tr data-index="' + i + '">';
+                                html += '<td class="product-col-name" data-bind="text:editModel.positions[' + i + '].product.name"></td>';
+                                html += '<td class="product-col-quantity"><input class="table-cell-input" type="number" data-bind="value:editModel.positions[' + i + '].quantity"/></td>';
+                                html += '<td class="product-col-unit" data-bind="text:editModel.positions[' + i + '].product.unit_name"></td>';
+                                html += '<td class="product-col-sum hide-on-small-only" data-bind="text:editModel.positions[' + i + '].sum"></td>';
+                                html += '<td class="product-col-btn"><a class="product-col-button-delete"><i class="material-icons editor-header">close</i></a></td>';
+                                html += '</tr>';
+                            }
+                        }
+                        html += '<tr>';
+                        html += '<td><a id="btn-add-position" class="btn btncol"><span data-bind="text:labelAdd"></span></a></td>';
+                        html += '</tr>';
+                        this.positionRows.html(html);
+                        self.Model.set("editModel", model);
+                        this.btnAddPosition = this.positionRows.find("#btn-add-composition");
+                        this.btnRemovePosition = this.positionRows.find(".editor-header-button");
+                        this.AddPositionButtonClick = this.createTouchClickEvent(this.btnAddPosition, this.addPositionButtonClick);
+                        this.RemovePositionButtonClick = this.createTouchClickEvent(this.btnRemovePosition, this.removePositionButtonClick);
+                        kendo.bind(this.positionRows, this.Model);
+                    };
+                    Editor.prototype.salePointButtonClick = function (e) {
+                        var self = this;
+                        vars._app.OpenController({
+                            urlController: 'setting/card/salepoint', isModal: true, onLoadController: function (controller) {
+                                var ctrlTypePayment = controller;
+                                ctrlTypePayment.CardSettings.IsAdd = false;
+                                ctrlTypePayment.CardSettings.IsAddCopy = false;
+                                ctrlTypePayment.CardSettings.IsDelete = false;
+                                ctrlTypePayment.CardSettings.IsEdit = false;
+                                ctrlTypePayment.CardSettings.IsSelect = true;
+                                ctrlTypePayment.OnSelect = $.proxy(self.selectSalePoint, self);
+                            }
+                        });
+                    };
+                    Editor.prototype.selectSalePoint = function (controller) {
+                        var salepoint = controller.getSelectedRecord();
+                        if (salepoint)
+                            this.Model.set("editModel.salepoint", salepoint);
+                        M.updateTextFields();
+                    };
+                    Editor.prototype.addPositionButtonClick = function (e) {
+                        var self = this;
+                        vars._app.OpenController({
+                            urlController: 'setting/card/product', isModal: true, onLoadController: function (controller) {
+                                var ctrlProduct = controller;
+                                ctrlProduct.CardSettings.IsAdd = false;
+                                ctrlProduct.CardSettings.IsEdit = false;
+                                ctrlProduct.CardSettings.IsDelete = false;
+                                ctrlProduct.CardSettings.IsSelect = true;
+                                ctrlProduct.OnSelect = $.proxy(self.selectPosition, self);
+                            }
+                        });
+                    };
+                    Editor.prototype.removePositionButtonClick = function (e) {
+                        var self = this;
+                        var model = this.EditorModel;
+                        var index = +$(e.currentTarget).parent().parent().data("index");
+                        model.positions.splice(index, 1);
+                        self.Model.set("editModel", model);
+                        self.setupPositions();
+                    };
+                    Editor.prototype.selectPosition = function (controller) {
+                        var id = controller.getSelectedRowId();
+                        var self = this;
+                        var model = this.EditorModel;
+                        self.SettingService.GetProductNewComposition(+id, function (responseData) {
+                            model.positions.push(responseData.newcomposition);
+                            self.Model.set("editModel", model);
+                            self.setupPositions();
+                        });
                     };
                     return Editor;
                 }(base.Controller.BaseEditor));
--- GriB.Client.App/Content/js/app/controller/document/editor/editor.ts
+++ GriB.Client.App/Content/js/app/controller/document/editor/editor.ts
@@ -1,5 +1,6 @@
 ﻿import base = require('app/common/basecontroller');
 import svc = require('app/services/documentservice');
+import svcSetting = require('app/services/settingsservice');
 import vars = require('app/common/variables');
 
 export namespace Controller.Document.Editor {
@@ -15,6 +16,13 @@ export namespace Controller.Document.Editor {
             return this.documentService;
         }
 
+        private settingService: svcSetting.Services.SettingsService;
+        protected get SettingService(): svcSetting.Services.SettingsService {
+            if (!this.settingService)
+                this.settingService = new svcSetting.Services.SettingsService();
+            return this.settingService;
+        }
+
         protected createOptions(): Interfaces.IControllerOptions {
             return { Url: "/Content/view/document/editor/document.html", Id: "document-view" };
         }
@@ -23,15 +31,15 @@ export namespace Controller.Document.Editor {
             let oo: kendo.data.ObservableObject = new kendo.data.ObservableObject({
                 "Header": this.Header,
                 "editModel": {},
-                //"labelAccessRight": vars._statres("label$accessright"),
-                //"labelCategory": vars._statres("label$category"),
-                //"labelName": vars._statres("label$name"),
-                //"labelIncludedInCategory": vars._statres("label$includedincategory"),
-                //"labelPosTerminal": vars._statres("label$POSterminal"),
-                //"labelAddPhoto": vars._statres("label$addphoto"),
-                //"labelDescription": vars._statres("label$description"),
-                //"labelSalePoint": vars._statres("label$salePoint"),
-                //"labelAccess": vars._statres("label$access"),
+                "conduct": true,
+                "labelConduct": vars._statres("label$conduct"),
+                "labelDate": vars._statres("label$date"),
+                "labelSalepoint": vars._statres("label$salePoint"),
+                "labelName": vars._statres("label$name"),
+                "labelQuantityShort": vars._statres("label$quantityshort"),
+                "labelUnitShort": vars._statres("label$unitshort"),
+                "labelSum": vars._statres("label$sum"),
+                "labelAdd": vars._statres("button$label$add")
             });
             return oo;
         }
@@ -56,7 +64,17 @@ export namespace Controller.Document.Editor {
             return result;
         }
 
+        protected dateControl: JQuery;
+        protected salePointControl: JQuery;
+        private positionRows: JQuery;
+        private btnAddPosition: JQuery;
+        private btnRemovePosition: JQuery;
         public ViewInit(view: JQuery): boolean {
+            this.dateControl = view.find("#document-view-date");
+            this.dateControl.datepicker();
+
+            this.salePointControl = view.find("#document-view-salepoint-row");
+            this.positionRows = view.find("#product-position-rows");
             return super.ViewInit(view);
         }
 
@@ -66,9 +84,19 @@ export namespace Controller.Document.Editor {
 
         protected createEvents(): void {
             super.createEvents();
+            this.SalePointButtonClick = this.createTouchClickEvent(this.salePointControl, this.salePointButtonClick);
+
+            this.Model.bind("change", $.proxy(this.changeModel, this));
        }
 
         protected destroyEvents(): void {
+            this.Model.unbind("change");
+            if (this.btnAddPosition)
+                this.destroyTouchClickEvent(this.btnAddPosition, this.addPositionButtonClick);
+            if (this.btnRemovePosition)
+                this.destroyTouchClickEvent(this.btnRemovePosition, this.removePositionButtonClick);
+
+            this.destroyTouchClickEvent(this.salePointControl, this.SalePointButtonClick);
             super.destroyEvents();
         }
 
@@ -79,11 +107,112 @@ export namespace Controller.Document.Editor {
 
         protected afterLoad(responseData?: any): void {
             super.afterLoad(responseData);
-            this.setupPositions(responseData);
+            this.setupPositions();
         }
 
-        private setupPositions(responseData: any) {
+        private changeModel(e: any): void {
+        }
+
+        private setupPositions() {
           
+            let self = this;
+            let model: Interfaces.Model.IDocumentModel = this.EditorModel;
+            let data: Interfaces.Model.IDocumentModelPosition[] = model.positions;
+            let html: string = '';
+
+            if (this.btnAddPosition)
+                this.destroyTouchClickEvent(this.btnAddPosition, this.addPositionButtonClick);
+
+            this.positionRows.unbind();
+            if (data && data.length > 0) {
+                for (let i = 0, icount = (data && data.length ? data.length : 0); i < icount; i++) {
+                    data[i].sum = Math.round((data[i].quantity * data[i].product.sellingprice) * 100) / 100;
+
+                    html += '<tr data-index="' + i + '">';
+                    html += '<td class="product-col-name" data-bind="text:editModel.positions[' + i + '].product.name"></td>';
+                    html += '<td class="product-col-quantity"><input class="table-cell-input" type="number" data-bind="value:editModel.positions[' + i + '].quantity"/></td>';
+                    html += '<td class="product-col-unit" data-bind="text:editModel.positions[' + i + '].product.unit_name"></td>';
+                    html += '<td class="product-col-sum hide-on-small-only" data-bind="text:editModel.positions[' + i + '].sum"></td>';
+                    html += '<td class="product-col-btn"><a class="product-col-button-delete"><i class="material-icons editor-header">close</i></a></td>';
+                    html += '</tr>';
+                }
+            }
+
+            html += '<tr>';
+            html += '<td><a id="btn-add-position" class="btn btncol"><span data-bind="text:labelAdd"></span></a></td>';
+            html += '</tr>';
+            this.positionRows.html(html);
+
+            self.Model.set("editModel", model);
+
+            this.btnAddPosition = this.positionRows.find("#btn-add-composition");
+            this.btnRemovePosition = this.positionRows.find(".editor-header-button");
+            this.AddPositionButtonClick = this.createTouchClickEvent(this.btnAddPosition, this.addPositionButtonClick);
+            this.RemovePositionButtonClick = this.createTouchClickEvent(this.btnRemovePosition, this.removePositionButtonClick);
+            kendo.bind(this.positionRows, this.Model);
+
+        }
+
+
+        public SalePointButtonClick: { (e: any): void; };
+        private salePointButtonClick(e) {
+            let self = this;
+
+            vars._app.OpenController({
+                urlController: 'setting/card/salepoint', isModal: true, onLoadController: (controller: Interfaces.IController) => {
+                    let ctrlTypePayment: Interfaces.IControllerCard = controller as Interfaces.IControllerCard;
+                    ctrlTypePayment.CardSettings.IsAdd = false;
+                    ctrlTypePayment.CardSettings.IsAddCopy = false;
+                    ctrlTypePayment.CardSettings.IsDelete = false;
+                    ctrlTypePayment.CardSettings.IsEdit = false;
+                    ctrlTypePayment.CardSettings.IsSelect = true;
+                    ctrlTypePayment.OnSelect = $.proxy(self.selectSalePoint, self);
+                }
+            });
+        }
+
+        private selectSalePoint(controller: Interfaces.IControllerCard) {
+            let salepoint: Interfaces.Model.ISalepoint = controller.getSelectedRecord() as Interfaces.Model.ISalepoint;
+            if (salepoint)
+                this.Model.set("editModel.salepoint", salepoint);
+            M.updateTextFields();
+        }
+
+        public AddPositionButtonClick: { (e: any): void; };
+        private addPositionButtonClick(e) {
+            let self = this;
+
+            vars._app.OpenController({
+                urlController: 'setting/card/product', isModal: true, onLoadController: (controller: Interfaces.IController) => {
+                    let ctrlProduct: Interfaces.IControllerCard = controller as Interfaces.IControllerCard;
+                    ctrlProduct.CardSettings.IsAdd = false;
+                    ctrlProduct.CardSettings.IsEdit = false;
+                    ctrlProduct.CardSettings.IsDelete = false;
+                    ctrlProduct.CardSettings.IsSelect = true;
+                    ctrlProduct.OnSelect = $.proxy(self.selectPosition, self);
+                }
+            });
+        }
+
+        public RemovePositionButtonClick: { (e: any): void; };
+        private removePositionButtonClick(e) {
+            let self = this;
+            let model: Interfaces.Model.IDocumentModel = this.EditorModel;
+            let index: number = +$(e.currentTarget).parent().parent().data("index");
+            model.positions.splice(index, 1);
+            self.Model.set("editModel", model);
+            self.setupPositions();
+        }
+
+        private selectPosition(controller: Interfaces.IControllerCard) {
+            let id: any = controller.getSelectedRowId();
+            let self = this;
+            let model: Interfaces.Model.IDocumentModel = this.EditorModel;
+            self.SettingService.GetProductNewComposition(+id, (responseData) => {
+                model.positions.push(responseData.newcomposition);
+                self.Model.set("editModel", model);
+                self.setupPositions();
+            });
         }
     }
 }
