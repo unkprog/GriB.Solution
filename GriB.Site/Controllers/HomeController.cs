d713ecc8f52eab754b7c0917b5f9cea091940588
Sergey Bezhnar <unkprog@gmail.com>
2019-06-07 09:56:58 +0300

    Обновление библиотеки materializecss

--- GriB.Site/Controllers/HomeController.cs
+++ GriB.Site/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace GriB.Site.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ GriB.Site/wwwroot/js/register.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+
+        function resizePlace() {
+
+            var header = document.getElementById("layout-header");
+            var footer = document.getElementById("layout-footer");
+
+            var element = document.getElementById("divImgBg");
+            element.style.height = 'calc(100hv - 64px)';
+
+
+        }
+
+        document.getElementsByTagName("BODY")[0].onresize = resizePlace;
+        resizePlace();
+
+})(); // end of jQuery name space
